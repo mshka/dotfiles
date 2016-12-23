117d8b77c29a5ffa8254b8d84c326f51bf9a9ca3
mark jad <mark.jad@live.com>
2016-12-23 16:11:26 +0200

    My identity for @lewagon in the gitconfig

--- install.sh
+++ install.sh
@@ -49,8 +49,8 @@ if [[ ! `uname` =~ "darwin" ]]; then
   git config --global core.editor "subl -n -w $@ >/dev/null 2>&1"
   echo 'export BUNDLER_EDITOR="subl $@ >/dev/null 2>&1"' >> zshrc
 else
-  git config --global core.editor "'/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl' -n -w"
-  bundler_editor="'/Applications/Sublime Text 2.app/Contents/SharedSupport/bin/subl'"
+  git config --global core.editor "'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' -n -w"
+  bundler_editor="'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl'"
   echo "export BUNDLER_EDITOR=\"${bundler_editor}\"" >> zshrc
 fi
 
